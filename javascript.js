62ecfdfa38010b86b41a166ac170f41b2d8b609c
Olamide Akinyemi <olamideakinyemi41@gmail.com>
2022-06-23 13:03:17 +0100

    update

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-function calc() {
+function calc(){
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
     var oper = document.getElementById("operators").value;
